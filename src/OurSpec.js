0e920b49c678286173fea9e41042088d03a52973
Shinikon <shinka4u@mail.ru>
2025-03-13 19:07:26 +0500

    Update OurSpec.js

--- src/OurSpec.js
+++ src/OurSpec.js
@@ -1,25 +1,22 @@
-
 const data = [
-    {
-        "id": 0,
-        "imageUrl": "./assets/img/spec1.svg",
-        "title": "@Shinka4u",
-        "descriptionn": "Архитектурный визуализатор"
-      },
+  {
+    id: 0,
+    imageUrl: '/assets/img/spec1.svg', // Путь, относительно public
+    title: "@Shinka4u",
+    descriptionn: "Архитектурный визуализатор",
+  },
+  {
+    id: 1,
+    imageUrl: '/assets/img/spec2.svg', // Путь, относительно public
+    title: "@Vantae",
+    descriptionn: "Универсальный специалист",
+  },
+  {
+    id: 2,
+    imageUrl: '/assets/img/spec3.svg', // Путь, относительно public
+    title: "@Yumeko",
+    descriptionn: "Геймдизайнер",
+  },
+];
 
-      {
-        "id": 1,
-        "imageUrl": "./assets/img/spec2.svg",
-        "title": "@Vantae",
-        "descriptionn": "Универсальный специалист"
-      },
-
-      {
-        "id": 2,
-        "imageUrl": "./assets/img/spec3.svg",
-        "title": "@Yumeko",
-        "descriptionn": "Геймдизайнер"
-      },
-  ];
-
-  export default data;
+export default data;
